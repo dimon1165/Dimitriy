b335d7736edfb1de303edd2f6ed2699ac4d305e0
Dmytro Voropai <dimon1165@gmail.com>
2016-09-19 21:55:03 +0300

    Enums/Threads

---
+++ src/learn/java/home/Threads/NewThread.java
@@ -0,0 +1,26 @@
+package learn.java.home.Threads;
+
+public class NewThread implements Runnable {
+	Thread thread;
+	
+	public NewThread() {
+//		новый поток исполнения 
+		thread = new Thread(this, "Demo thread");
+		System.out.println("Inherited trhread was created" + thread);
+		thread.start();
+	} 
+	
+	@Override
+	public void run() {
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println("Inherited thread " + i);
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("End of inherited thread");
+	}
+	
+}
