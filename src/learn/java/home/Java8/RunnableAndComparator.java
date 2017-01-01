83a42a73a7c5e4209927bd50fca123d76ef6a15a
Voropai_Dmytro <dimon1165@gmail.com>
2017-01-02 01:11:15 +0200

    Lambda Runnable small changes

--- src/learn/java/home/Java8/RunnableAndComparator.java
+++ src/learn/java/home/Java8/RunnableAndComparator.java
@@ -3,8 +3,7 @@ package learn.java.home.Java8;
 import learn.java.home.Threads.NewThread;
 
 /**
- *
- *
+ * Как пример для интерфейса Runnable
  */
 public class RunnableAndComparator {
     public static void main(String[] args) throws InterruptedException {
