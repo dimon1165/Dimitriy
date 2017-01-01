63dd00f16292d599b7e17a34a34388e922c5ffb2
Voropai_Dmytro <dimon1165@gmail.com>
2017-01-02 01:07:30 +0200

    Lambda Runnable

--- src/learn/java/home/Java8/RunnableAndComparator.java
+++ src/learn/java/home/Java8/RunnableAndComparator.java
@@ -1,7 +1,39 @@
 package learn.java.home.Java8;
 
+import learn.java.home.Threads.NewThread;
+
 /**
+ *
  *
  */
 public class RunnableAndComparator {
+    public static void main(String[] args) throws InterruptedException {
+        /*
+         * Запись без лямбда выражения
+         **/
+
+//  Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                int i;
+//                for (i = 0; i <= 3; i++) {
+//                    System.out.println("Hello world form thread [ " + Thread.currentThread().getName() + " ]");
+//                }
+//            }
+//  };
+
+
+        /*
+        * Запись с лямбда выражением
+        * */
+        Runnable runnable = () -> {int i;
+                for (i = 0; i <= 3; i++) {
+                    System.out.println("Hello world form thread [ " + Thread.currentThread().getName() + " ]");
+                }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+        thread.join();
+    }
 }
