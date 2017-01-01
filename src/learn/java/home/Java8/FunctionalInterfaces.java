9e0db83aafa36bd22334330f07feed4ad032dac7
Voropai_Dmytro <dimon1165@gmail.com>
2017-01-02 01:57:28 +0200

    Lambda Functional Interfaces

---
+++ src/learn/java/home/Java8/FunctionalInterfaces.java
@@ -0,0 +1,31 @@
+package learn.java.home.Java8;
+
+/**
+ * Разделяются на 4 категории:
+ * 1. SUPPLIER: не принимает никаких объектов,только создает их.
+ *         @FunctionalInterface
+ *         public interface Supplier<T>{
+ *           T get();
+ *         }
+ *
+ * 2. CONSUMER/BiCONSUMER: принимает объкты, но ничего не возвращает
+ *         @FunctionalInterface
+ *         public interface Consumer<T>{
+ *           void accept(T t);
+ *         }
+ *
+ * 3. PREDICATE/BiPREDICATE: принимает объект в качестве параметра и возвращает boolean
+ *         @FunctionalInterface
+ *         public interface Predicate<T>{
+ *           boolean test(T t);
+ *         }
+ * 4. FUNCTION/BiFUNCTION: принимает объект в качестве параметра и возвращает объект.
+ *         @FunctionalInterface
+ *         public interface Function<T, R>{
+ *           R apply(T t);
+ *         }
+ *
+ */
+public class FunctionalInterfaces {
+
+}
