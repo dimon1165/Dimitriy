2c18498feb4ae19757fe6930fdecec600e3b0528
Voropai_Dmytro <dimon1165@gmail.com>
2017-01-08 20:04:44 +0200

    JavaIO8

--- src/learn/java/home/Java8/JavaIOStringsAndOthersAnnotationsMaps/JavaIOAPI.java
+++ src/learn/java/home/Java8/JavaIOStringsAndOthersAnnotationsMaps/JavaIOAPI.java
@@ -1,21 +1,42 @@
 package learn.java.home.Java8.JavaIOStringsAndOthersAnnotationsMaps;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 /**
- *
+ * How Java 8 IO works
  */
 public class JavaIOAPI {
     public static void main(String[] args) {
-        try(
-                BufferedReader bufferedReader = new BufferedReader(
-                        new FileReader((new File("C:\\Users\\Nadya\\projects\\Dimitriy_Study\\peoples.txt"))));
-                ){
+        try(BufferedReader bufferedReader =
+                    new BufferedReader(
+                        new FileReader(
+                                new File("/Users/Voropai_Dmytro/git/LearningJava/src/" +
+                                        "learn/java/home/Java8/JavaIOStringsAndOthersAnnotationsMaps/peoples.txt")))
+        ){
+            Stream<String> stream = bufferedReader.lines();
+            stream.filter(line -> line.contains("Amy"))
+                    .findFirst()
+                    .ifPresent(System.out::println);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //Мы можем исследовать файловую систему с методом Path:
+        //isDirectory возвращает path к файлу и какие в нем директории
+        //isFile возвращает path к файлу и файлы внутри
+        Path path = Paths.get("/Users/Voropai_Dmytro/git/LearningJava/src/" +
+                                    "learn/java/home/Java8/JavaIOStringsAndOthersAnnotationsMaps/");
+                try(Stream<Path> stream = Files.list(path)){
+                    stream.filter(path1 -> path1.toFile().isFile())
+                            .forEach(System.out::println);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
     }
 }
