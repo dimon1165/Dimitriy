02334b50457eb27cfe5d5fcba8936f7bc9e4ec6c
Voropai_Dmytro <dimon1165@gmail.com>
2017-01-22 15:00:01 +0200

    postgresql

--- src/learm/java/jdbc/JavaToPostgres.java
+++ src/learm/java/jdbc/JavaToPostgres.java
@@ -9,12 +9,19 @@ import java.sql.SQLException;
  * Created by Voropai_Dmytro on 22.01.17.
  */
 public class JavaToPostgres {
+
+    private static final String JDBC_POSTGRESQL_URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String PASSWORD = "";
+    public static final String USER = "";
+
     public static void main(String[] args) {
             Connection connection = null;
         try {
-            connection = DriverManager.getConnection("","dimon1165","Marot1165");
+            connection = DriverManager.getConnection(JDBC_POSTGRESQL_URL,
+                    USER, PASSWORD);
         } catch (SQLException e) {
             System.out.println("The error message is: " + e.getMessage());
+
         }
 
     }
