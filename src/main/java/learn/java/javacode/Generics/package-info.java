084cdb4fe3057bc0dab7c440d82c30ebdf543935
Dmytro Voropai <dvorop@softserveinc.com>
2017-02-16 20:43:35 +0200

    Generics general info

---
+++ src/main/java/learn/java/javacode/Generics/package-info.java
@@ -0,0 +1,11 @@
+/**
+ *  Created by dvorop on 16.02.2017.
+ *  Generics wildcards(types):
+ *  1. Upper Bounded  - List<? extends Class> - except anything which is a subclass of Class.
+ *  2. Lower Bounded  - List<? super Class> - except anything which is the Class
+ *  or Parent of the Class.
+ *  3. Unbounded - List<?> - anything.
+ *
+ *
+ */
+package learn.java.javacode.Generics;
