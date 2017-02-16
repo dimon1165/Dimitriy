09875ee632243f85ca75f812556b1dd65abfc6c9
Dmytro Voropai <dvorop@softserveinc.com>
2017-02-16 20:06:09 +0200

    small modifications

--- src/main/java/learn/java/javacode/Collections/package-info.java
+++ src/main/java/learn/java/javacode/Collections/package-info.java
@@ -10,7 +10,7 @@
  *  2. Set(elements appears only once in collection)
  *  2.0.1 HashSet
  *
- *  2.1 SortedSet ( SortedSet -sort order).
+ *  2.1 SortedSet ( SortedSet - sort order).
  *  2.1.1 TreeSet
  *
  *  3. Queue (order - first in first out)
@@ -20,7 +20,7 @@
  *  3.1.1 LinkedList
  *  3.1.2 ArrayDeque
  *
- *  4. Map(Key/value)
+ *  4. Map(Key unique/value)
  *  4.0.1 HashMap
  *  4.1 SortedMap
  *  4.1.1 TreeMap
