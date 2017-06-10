6cac4427bb8b6d9bd5be358d6c06e614a3232b95
Voropai_Dmytro <dimon1165@gmail.com>
2017-06-10 22:05:20 +0300

    info package

---
+++ src/main/java/learn/java/javacode/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Voropai_Dmytro on 30.01.17.
+ * This package has purpose cover general questions about java
+ * 1. if-else statement in another sign:
+ * result = condition ? true value : false value;
+ *
+ */
+package learn.java.javacode;
